ed16cde1953126d7049886736070cbea999eb8cf
Crew Taylor <crewtaylor@MacBook-Pro.local>
2020-11-13 10:15:35 -0800

    refactored utils

---
+++ util_falcon.py
@@ -0,0 +1,92 @@
+args = [
+    {
+        "csection": "sphere",
+        "radius": 1.75,
+        "mass": 549054,
+        "density": 1.274,
+        "starting_velocity": 1000,
+        "notes": "Earth Falcon 9 Sphere",
+        "g": 9.8
+
+    },
+    {
+        "csection": "sphere",
+        "radius": 1.75,
+        "mass": 549054,
+        "density": .02,
+        "starting_velocity": 1000,
+        "notes": "Mars Falcon 9 Sphere",
+        "g": 3.7
+
+    },
+    {
+        "csection": "sphere",
+        "radius": 1.75,
+        "mass": 549054,
+        "density": 65,
+        "starting_velocity": 1000,
+        "notes": "Venus Falcon 9 Sphere",
+        "g": 8.9
+    },
+    {
+        "csection": "paraboloid",
+        "radius": 1.75,
+        "height": 70,
+        "mass": 549054,
+        "density": 1.274,
+        "starting_velocity": 1000,
+        "notes": "Earth Falcon 9 Paraboloid",
+        "g": 9.8
+
+    },
+    {
+        "csection": "paraboloid",
+        "radius": 1.75,
+        "height": 70,
+        "mass": 549054,
+        "density": .02,
+        "starting_velocity": 1000,
+        "notes": "Mars Falcon 9 Paraboloid",
+        "g": 3.7
+
+    },
+    {
+        "csection": "paraboloid",
+        "radius": 1.75,
+        "height": 70,
+        "mass": 549054,
+        "density": 65,
+        "starting_velocity": 1000,
+        "notes": "Venus Falcon 9 Paraboloid",
+        "g": 8.9
+    },
+    {
+        "csection": "elipsoid",
+        "radius": 1.75,
+        "mass": 549054,
+        "density": 1.274,
+        "starting_velocity": 1000,
+        "notes": "Earth Falcon 9 Elipsoid",
+        "g": 9.8
+
+    },
+    {
+        "csection": "elipsoid",
+        "radius": 1.75,
+        "mass": 549054,
+        "density": .02,
+        "starting_velocity": 1000,
+        "notes": "Mars Falcon 9 Elipsoid",
+        "g": 3.7
+
+    },
+    {
+        "csection": "elipsoid",
+        "radius": 1.75,
+        "mass": 549054,
+        "density": 65,
+        "starting_velocity": 1000,
+        "notes": "Venus Falcon 9 Elipsoid",
+        "g": 8.9
+    }
+]
